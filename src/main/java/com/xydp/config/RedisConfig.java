116ea3ac6c01d3ffda2b7f53a6ddde3150f0a590
jasper_lap <1836346913@qq.com>
2024-07-18 23:26:12 +0800

    Initial commit

---
+++ src/main/java/com/xydp/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.xydp.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redisson 客户端类
+ */
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedissonClient redissonClient() {
+        // 配置类
+        Config config = new Config();
+        // 添加redis地址，这里添加了单点的地址，也可以使用config.useClusterServers()添加集群地址
+//        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setPassword("123456");
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379"); // Redis没有密码时不需要设置password参数
+        // 创建客户端
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public RedissonClient redissonClient2() {
+        // 配置类
+        Config config = new Config();
+        // 添加redis地址，这里添加了单点的地址，也可以使用config.useClusterServers()添加集群地址
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        // 创建客户端
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public RedissonClient redissonClient3() {
+        // 配置类
+        Config config = new Config();
+        // 添加redis地址，这里添加了单点的地址，也可以使用config.useClusterServers()添加集群地址
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        // 创建客户端
+        return Redisson.create(config);
+    }
+}
---
+++ src/main/java/com/xydp/service/ITokenService.java
@@ -0,0 +1,20 @@
+package com.xydp.service;
+
+/**
+ * Token 服务类接口
+ *
+ * @author Wuxy
+ * @version 1.0
+ * @ClassName TokenService
+ * @since 2023/6/3 17:32
+ */
+public interface ITokenService {
+
+    /**
+     * 下单前获取令牌用户防重提交
+     *
+     * @return 返回生成的 token
+     */
+    String getOrderToken();
+
+}
